6a57e441286e33790f0d884ce71c298fe66a2212
unknown <yuting813@gmail.com>
2024-05-03 01:51:49 +0800

    Set API URL

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,11 +1,15 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/user";
+// const API_UserURL = "http://localhost:8080/api/user";
 // const API_URL = "https://mern-project-p7hk.onrender.com/api/user";
+const API_UserURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080/api/user"
+    : "https://mern-project-p7hk.onrender.com/api/user";
 // 跟登入有關的服務物件
 
 class AuthService {
   login(email, password) {
-    return axios.post(API_URL + "/login", {
+    return axios.post(API_UserURL + "/login", {
       email,
       password,
     });
@@ -14,7 +18,7 @@ class AuthService {
     localStorage.removeItem("user");
   }
   register(username, email, password, role) {
-    return axios.post(API_URL + "/register", {
+    return axios.post(API_UserURL + "/register", {
       username,
       email,
       password,
--- client/src/services/course.service.js
+++ client/src/services/course.service.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 // import { JSON } from "react-router-dom";
-const API_URL = "http://localhost:8080/api/courses";
-// const API_URL = "hhttps://mern-project-p7hk.onrender.com/api/courses";
+// const API_CoursesURL = "http://localhost:8080/api/courses";
+// const API_CoursesURL = "hhttps://mern-project-p7hk.onrender.com/api/courses";
+const API_CoursesURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080/api/courses"
+    : "https://mern-project-p7hk.onrender.com/api/user";
 
 // 定義獲取token的函數
 function getToken() {
@@ -13,7 +17,7 @@ class CourseService {
   post(title, description, price) {
     const token = getToken();
     return axios.post(
-      API_URL,
+      API_CoursesURL,
       { title, description, price },
       {
         headers: {
@@ -27,7 +31,7 @@ class CourseService {
   async getEnrolledCourses(_id) {
     const token = getToken();
     try {
-      const response = await axios.get(API_URL + "/student/" + _id, {
+      const response = await axios.get(API_CoursesURL + "/student/" + _id, {
         headers: {
           Authorization: token,
         },
@@ -44,7 +48,7 @@ class CourseService {
   // 使用instructor id,來找到講師擁有的課程
   async get(_id) {
     const token = getToken();
-    const response = await axios.get(API_URL + "/instructor/" + _id, {
+    const response = await axios.get(API_CoursesURL + "/instructor/" + _id, {
       headers: {
         Authorization: token,
       },
@@ -62,7 +66,7 @@ class CourseService {
   // 使用課程名稱搜尋課程
   // getCourseByName(name) {
   //   const token = getToken();
-  //   return axios.get(API_URL + "/findByName/" + name, {
+  //   return axios.get(API_CoursesURL + "/findByName/" + name, {
   //     headers: {
   //       Authorization: token,
   //     },
@@ -72,7 +76,7 @@ class CourseService {
   // 使用课程名称模糊搜索課程
   getCourseByName(name) {
     const token = getToken();
-    return axios.get(API_URL + /search/ + name, {
+    return axios.get(API_CoursesURL + /search/ + name, {
       headers: {
         Authorization: token,
       },
@@ -83,7 +87,7 @@ class CourseService {
   enroll(_id) {
     const token = getToken();
     return axios.post(
-      API_URL + "/enroll/" + _id,
+      API_CoursesURL + "/enroll/" + _id,
       {},
       {
         headers: {
