5db687cbe2d4d51d275adb204356bcec68c0c6a0
unknown <yuting813@gmail.com>
2024-05-03 18:41:10 +0800

    Set render-postbuild

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+// app.use(express.static(path.join(__dirname, "/client/build")));
+// 提供靜態檔案服務
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use("/api/user", authRoute);
 
@@ -54,7 +56,7 @@ if (process.env.NODE_ENV === "production") {
 
   // app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
   app.get("*", (req, res) =>
-    res.sendFile(path.join(process.cwd(), "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "build", "index.html"))
   );
 }
 
